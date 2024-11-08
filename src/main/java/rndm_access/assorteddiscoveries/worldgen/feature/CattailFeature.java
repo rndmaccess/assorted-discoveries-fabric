323af0c888485044180ab71e7bf32c5d65ded5fb
rndmaccess <rndm.access.dev@gmail.com>
2024-11-08 00:38:40 -0700

    Cattails no longer spawn in icy biomes where the water has frozen over!

--- src/main/java/rndm_access/assorteddiscoveries/worldgen/feature/CattailFeature.java
+++ src/main/java/rndm_access/assorteddiscoveries/worldgen/feature/CattailFeature.java
@@ -31,8 +31,9 @@ public class CattailFeature extends Feature<ProbabilityConfig> {
         BlockState lowerHalf = ModBlocks.CATTAIL.getDefaultState();
         BlockState upperHalf = lowerHalf.with(CattailBlock.HALF, DoubleBlockHalf.UPPER);
         boolean canPlace = lowerHalf.canPlaceAt(world, lowerPos) && world.getBlockState(upperPos).isAir();
+        boolean isCold = world.getBiome(lowerPos).value().getTemperature() <= 0.0F;
 
-        if (canPlace) {
+        if (canPlace && !isCold) {
             world.setBlockState(lowerPos, lowerHalf.with(CattailBlock.WATERLOGGED, world.isWater(lowerPos)), 2);
             world.setBlockState(upperPos, upperHalf, 2);
             return true;

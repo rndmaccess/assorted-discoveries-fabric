5cc1200f857bf49827f08e0b9ec0092c7a71be83
rndm_access <rndm.access.dev@gmail.com>
2025-06-21 22:45:30 -0600

    Most of the config error screen can now be translated using the game language files!

--- src/client/java/rndm_access/assorteddiscoveries/config/ModConfigErrorScreen.java
+++ src/client/java/rndm_access/assorteddiscoveries/config/ModConfigErrorScreen.java
@@ -5,6 +5,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import rndm_access.assorteddiscoveries.ADReference;
 
 public class ModConfigErrorScreen extends Screen {
     private final Screen parent;
@@ -21,23 +22,28 @@ public class ModConfigErrorScreen extends Screen {
 
         final MultilineText multilineError = MultilineText.create(textRenderer,
                 Text.literal(ModConfig.getConfigError()), width - 80);
+        Text desc_line_one = Text.translatable("config_error." + ADReference.MOD_ID
+                + ".screen.description.line_one");
+        Text desc_line_two = Text.translatable("config_error." + ADReference.MOD_ID
+                + ".screen.description.line_two");
+        Text desc_line_three = Text.translatable("config_error." + ADReference.MOD_ID
+                + ".screen.description.line_three");
 
         context.drawCenteredTextWithShadow(textRenderer, title, width / 2, 20, 0xffffff);
-        context.drawCenteredTextWithShadow(textRenderer,
-                Text.literal("The config could not be loaded because of the following error,"),
-                width / 2, height / 2 - 80, 0xffffff);
-        context.drawCenteredTextWithShadow(textRenderer,
-                Text.literal("please fix this error at the specified path before continuing!"),
-                width / 2, height / 2 - 65, 0xffffff);
-        context.drawCenteredTextWithShadow(textRenderer,
-                Text.literal("Assorted Discoveries has fallen back to using all default config options."),
-                width / 2, height / 2 - 50, 0xffffff);
+        context.drawCenteredTextWithShadow(textRenderer, desc_line_one, width / 2,
+                height / 2 - 80, 0xffffff);
+        context.drawCenteredTextWithShadow(textRenderer, desc_line_two, width / 2,
+                height / 2 - 65, 0xffffff);
+        context.drawCenteredTextWithShadow(textRenderer, desc_line_three, width / 2,
+                height / 2 - 50, 0xffffff);
         multilineError.drawWithShadow(context, 30, height / 2 - 30, 16, 0xff5555);
     }
 
     @Override
     protected void init() {
-        this.closeButton = ButtonWidget.builder(Text.literal("Close"), button -> this.close())
+        this.closeButton = ButtonWidget
+                .builder(Text.translatable("config_error." + ADReference.MOD_ID + ".screen.close_button"),
+                        button -> this.close())
                 .dimensions(width / 2 - 50, height / 4 + height / 2 + 40, 100, 20).build();
         addDrawableChild(closeButton);
     }
--- src/client/java/rndm_access/assorteddiscoveries/config/ModMenuIntegration.java
+++ src/client/java/rndm_access/assorteddiscoveries/config/ModMenuIntegration.java
@@ -5,13 +5,15 @@ import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.text.Text;
+import rndm_access.assorteddiscoveries.ADReference;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         if (ModConfig.getConfigError() != null) {
-            return parent -> new ModConfigErrorScreen(Text.literal("Assorted Discoveries Config Errors"), parent);
+            Text config_errors_title = Text.translatable("config_error." + ADReference.MOD_ID + ".screen.title");
+            return parent -> new ModConfigErrorScreen(config_errors_title, parent);
         }
         return parent -> ModConfigScreen.getConfigScreenBuilder().setParentScreen(parent).build();
     }

9b87c0e954c9418cabd982c020bf5e4ab874f5dd
rndm_access <rndm.access.dev@gmail.com>
2025-08-03 23:18:46 -0600

    The token class no longer uses a builder simplifying the code!

--- src/main/java/rndm_access/assorteddiscoveries/config/json/tokenizer/Token.java
+++ src/main/java/rndm_access/assorteddiscoveries/config/json/tokenizer/Token.java
@@ -5,10 +5,10 @@ public class Token {
     private final String value;
     private final int line;
 
-    protected Token(Token.Builder builder) {
-        this.type = builder.type;
-        this.value = builder.value;
-        this.line = builder.line;
+    protected Token(TokenType type, String value, int line) {
+        this.type = type;
+        this.value = value;
+        this.line = line;
     }
 
     public TokenType getType() {
@@ -21,30 +21,5 @@ public class Token {
 
     public int getLine() {
         return line;
-    }
-
-    public static class Builder {
-        private TokenType type;
-        private String value;
-        private int line;
-
-        public Builder setType(TokenType type) {
-            this.type = type;
-            return this;
-        }
-
-        public Builder setValue(String value) {
-            this.value = value;
-            return this;
-        }
-
-        public Builder setLine(int line) {
-            this.line = line;
-            return this;
-        }
-
-        public Token build() {
-            return new Token(this);
-        }
     }
 }

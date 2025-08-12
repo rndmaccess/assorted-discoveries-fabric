8d56d6b314ec13a807086e24ef971cedec19a2cc
rndm_access <rndm.access.dev@gmail.com>
2025-08-12 11:25:06 -0600

    Rewrote the comment in the client config class!

--- src/main/java/rndm_access/assorteddiscoveries/config/ModClientConfig.java
+++ src/main/java/rndm_access/assorteddiscoveries/config/ModClientConfig.java
@@ -3,9 +3,10 @@ package rndm_access.assorteddiscoveries.config;
 import java.util.Map;
 
 /**
- * This config is sent from the server to the client whenever a player joins the game and is updated on
+ * This config is used in client based places in the code such as creative tabs to reflect the server config data.
+ * The config is populated on the client whenever a player joins the game and is updated on
  * the server whenever a server starts! Currently, it's only used to decide what items should be
- * visible in the creative tab!
+ * visible in the assorted discoveries creative tab!
  */
 public class ModClientConfig {
     private static volatile Map<String, Boolean> boolServerConfigEntries = null;

2d7107edbad4281e15c87f861e0cb77bb84bb34e
rndm_access <rndm.access.dev@gmail.com>
2025-09-02 11:25:12 -0600

    Mobs should pathfind through the damaging bushes less hopefully!

--- src/main/java/rndm_access/assorteddiscoveries/block/AbstractBerryBushBlock.java
+++ src/main/java/rndm_access/assorteddiscoveries/block/AbstractBerryBushBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.block.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCollisionHandler;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.pathing.NavigationType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -161,6 +162,11 @@ public abstract class AbstractBerryBushBlock extends PlantBlock implements Ferti
             ++amount;
         }
         return amount;
+    }
+
+    @Override
+    protected boolean canPathfindThrough(BlockState state, NavigationType type) {
+        return !this.bushDamages();
     }
 
     static {

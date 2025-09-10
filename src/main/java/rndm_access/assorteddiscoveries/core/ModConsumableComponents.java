a9641b29bf5eacbfc7c68bdb543090df99e40474
rndm_access <rndm.access.dev@gmail.com>
2025-09-10 10:24:33 -0600

    In lined all the registries removing the need for the static init blocks!

--- src/main/java/rndm_access/assorteddiscoveries/core/ModConsumableComponents.java
+++ src/main/java/rndm_access/assorteddiscoveries/core/ModConsumableComponents.java
@@ -7,17 +7,12 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 
 public final class ModConsumableComponents {
-    public static final ConsumableComponent WITCHS_CRADLE_SOUP;
-    public static final ConsumableComponent NETHER_FOOD;
-
-    static {
-        WITCHS_CRADLE_SOUP = ConsumableComponents.food()
-                .consumeEffect(new ApplyEffectsConsumeEffect(
-                        new StatusEffectInstance(StatusEffects.NIGHT_VISION, 2400, 0,
-                                true, true))).build();
-        NETHER_FOOD = ConsumableComponents.drink()
-                .consumeEffect(new ApplyEffectsConsumeEffect(
-                        new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 2400,
-                                0, true, true))).build();
-    }
+    public static final ConsumableComponent WITCHS_CRADLE_SOUP = ConsumableComponents.food()
+            .consumeEffect(new ApplyEffectsConsumeEffect(
+                    new StatusEffectInstance(StatusEffects.NIGHT_VISION, 2400, 0,
+                            true, true))).build();
+    public static final ConsumableComponent NETHER_FOOD = ConsumableComponents.drink()
+            .consumeEffect(new ApplyEffectsConsumeEffect(
+                    new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 2400,
+                            0, true, true))).build();
 }
--- src/main/java/rndm_access/assorteddiscoveries/core/ModFoodComponents.java
+++ src/main/java/rndm_access/assorteddiscoveries/core/ModFoodComponents.java
@@ -3,43 +3,36 @@ package rndm_access.assorteddiscoveries.core;
 import net.minecraft.component.type.FoodComponent;
 
 public final class ModFoodComponents {
-    public static final FoodComponent FRIED_EGG;
-    public static final FoodComponent GREEN_ONION;
-    public static final FoodComponent NOODLE_SOUP;
-    public static final FoodComponent PUDDING;
-    public static final FoodComponent BERRY_PUDDING;
-    public static final FoodComponent CARAMEL;
-    public static final FoodComponent CARAMEL_APPLE;
-    public static final FoodComponent WITCHS_CRADLE_BRANCH;
-    public static final FoodComponent WITCHS_CRADLE_SOUP;
-    public static final FoodComponent BLUEBERRIES;
-    public static final FoodComponent JUICE;
-    public static final FoodComponent SPRUCE_CONE;
-    public static final FoodComponent FORESTS_BOUNTY;
-    public static final FoodComponent HOGLIN_STEW;
-    public static final FoodComponent NETHER_BERRIES;
-    public static final FoodComponent NETHER_FORAGE;
+    public static final FoodComponent FRIED_EGG = (new FoodComponent.Builder()).nutrition(5)
+            .saturationModifier(0.6F).build();
+    public static final FoodComponent GREEN_ONION = new FoodComponent.Builder().nutrition(1)
+            .saturationModifier(0.6F).build();
+    public static final FoodComponent NOODLE_SOUP = createBasicStew(8);
+    public static final FoodComponent PUDDING = createBasicStew(3);
+    public static final FoodComponent BERRY_PUDDING = createBasicStew(6);
+    public static final FoodComponent CARAMEL = new FoodComponent.Builder().nutrition(2)
+            .saturationModifier(0.3F).build();
+    public static final FoodComponent CARAMEL_APPLE = new FoodComponent.Builder().nutrition(4)
+            .saturationModifier(0.6F).build();
+    public static final FoodComponent WITCHS_CRADLE_BRANCH = new FoodComponent.Builder().nutrition(2)
+            .saturationModifier(0.1F).build();
+    public static final FoodComponent WITCHS_CRADLE_SOUP = new FoodComponent.Builder().nutrition(6)
+            .saturationModifier(0.6F).alwaysEdible().build();
+    public static final FoodComponent BLUEBERRIES = new FoodComponent.Builder().nutrition(2)
+            .saturationModifier(0.1F).build();
+    public static final FoodComponent JUICE = new FoodComponent.Builder().nutrition(6)
+            .saturationModifier(0.6F).build();
+    public static final FoodComponent SPRUCE_CONE = new FoodComponent.Builder().nutrition(2)
+            .saturationModifier(0.3F).build();
+    public static final FoodComponent FORESTS_BOUNTY = createBasicStew(5);
+    public static final FoodComponent HOGLIN_STEW = new FoodComponent.Builder().nutrition(8)
+            .saturationModifier(0.8F).build();
+    public static final FoodComponent NETHER_BERRIES = new FoodComponent.Builder().nutrition(3)
+            .saturationModifier(0.2F).build();
+    public static final FoodComponent NETHER_FORAGE = new FoodComponent.Builder().nutrition(8)
+            .saturationModifier(0.8F).build();
 
     private static FoodComponent createBasicStew(int hunger) {
         return new FoodComponent.Builder().nutrition(hunger).saturationModifier(0.6F).build();
-    }
-
-    static {
-        FRIED_EGG = (new FoodComponent.Builder()).nutrition(5).saturationModifier(0.6F).build();
-        GREEN_ONION = new FoodComponent.Builder().nutrition(1).saturationModifier(0.6F).build();
-        NOODLE_SOUP = createBasicStew(8);
-        PUDDING = createBasicStew(3);
-        BERRY_PUDDING = createBasicStew(6);
-        CARAMEL = new FoodComponent.Builder().nutrition(2).saturationModifier(0.3F).build();
-        CARAMEL_APPLE = new FoodComponent.Builder().nutrition(4).saturationModifier(0.6F).build();
-        WITCHS_CRADLE_BRANCH = new FoodComponent.Builder().nutrition(2).saturationModifier(0.1F).build();
-        WITCHS_CRADLE_SOUP = new FoodComponent.Builder().nutrition(6).saturationModifier(0.6F).alwaysEdible().build();
-        BLUEBERRIES = new FoodComponent.Builder().nutrition(2).saturationModifier(0.1F).build();
-        JUICE = new FoodComponent.Builder().nutrition(6).saturationModifier(0.6F).build();
-        SPRUCE_CONE = new FoodComponent.Builder().nutrition(2).saturationModifier(0.3F).build();
-        FORESTS_BOUNTY = createBasicStew(5);
-        HOGLIN_STEW = new FoodComponent.Builder().nutrition(8).saturationModifier(0.8F).build();
-        NETHER_BERRIES = new FoodComponent.Builder().nutrition(3).saturationModifier(0.2F).build();
-        NETHER_FORAGE = new FoodComponent.Builder().nutrition(8).saturationModifier(0.8F).build();
     }
 }

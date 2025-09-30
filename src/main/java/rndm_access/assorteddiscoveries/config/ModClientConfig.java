7f0e60a01b613990121ed2cee858975e2ae08665
rndm_access <rndm.access.dev@gmail.com>
2025-09-30 11:51:22 -0600

    Added a bit of explanation in the client config explaining more about how it is a read-only view!

--- src/main/java/rndm_access/assorteddiscoveries/config/ModClientConfig.java
+++ src/main/java/rndm_access/assorteddiscoveries/config/ModClientConfig.java
@@ -6,7 +6,7 @@ import java.util.Map;
  * This config is used in client based places in the code such as creative tabs to reflect the server config data.
  * The config is populated on the client whenever a player joins the game and is updated on
  * the server whenever a server starts! Currently, it's only used to decide what items should be
- * visible in the assorted discoveries creative tab!
+ * visible in the assorted discoveries creative tab! This representation of the config is a read-only view!
  */
 public class ModClientConfig {
     private static volatile Map<String, Boolean> boolServerConfigEntries = null;

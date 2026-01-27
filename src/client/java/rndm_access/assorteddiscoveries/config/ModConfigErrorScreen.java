94e64506a93cab2d81d87cc963c1aead28bacbd4
rndm_access <rndm.access.dev@gmail.com>
2026-01-26 23:05:51 -0700

    Updated and improved the error config screen! It now has functionality to overrite existing config files if the user chooses to do that!

--- src/client/java/rndm_access/assorteddiscoveries/config/ModConfigErrorScreen.java
+++ src/client/java/rndm_access/assorteddiscoveries/config/ModConfigErrorScreen.java
@@ -4,13 +4,25 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.FormattedText;
 import net.minecraft.network.chat.Style;
+import org.jspecify.annotations.NonNull;
 import rndm_access.assorteddiscoveries.AssortedDiscoveries;
 
 public class ModConfigErrorScreen extends Screen {
+    private static final int DEFAULT_TEXT_COLOR = 0xFFFFFFFF;
+    private static final int ERROR_TEXT_COLOR = 0xFF5555;
     private final Screen parent;
-    public Button closeButton;
+    public Button goBackButton;
+    public Button continueButton;
+    private final Component desc_line_one = Component.translatable("config_error." + AssortedDiscoveries.MOD_ID
+            + ".screen.description.line_one");
+    private final Component desc_line_two = Component.translatable("config_error." + AssortedDiscoveries.MOD_ID
+            + ".screen.description.line_two");
+    private final Component desc_line_three = Component.translatable("config_error." + AssortedDiscoveries.MOD_ID
+            + ".screen.description.line_three");
+    private final Component desc_line_four = Component.translatable("config_error." + AssortedDiscoveries.MOD_ID
+            + ".screen.description.line_four");
 
     protected ModConfigErrorScreen(Component title, Screen parent) {
         super(title);
@@ -18,37 +30,41 @@ public class ModConfigErrorScreen extends Screen {
     }
 
     @Override
-    public void render(GuiGraphics gui, int mouseX, int mouseY, float deltaTicks) {
+    public void render(@NonNull GuiGraphics gui, int mouseX, int mouseY, float deltaTicks) {
         super.render(gui, mouseX, mouseY, deltaTicks);
-        MutableComponent errorText = Component.literal(ModServerConfig.getConfigError());
-        errorText.setStyle(Style.EMPTY.withColor(0xff5555));
-        //final MultilineText multilineError = FittingMultiLineTextWidget.create(textRenderer, errorText, width - 80);
-        Component desc_line_one = Component.translatable("config_error." + AssortedDiscoveries.MOD_ID
-                + ".screen.description.line_one");
-        Component desc_line_two = Component.translatable("config_error." + AssortedDiscoveries.MOD_ID
-                + ".screen.description.line_two");
-        Component desc_line_three = Component.translatable("config_error." + AssortedDiscoveries.MOD_ID
-                + ".screen.description.line_three");
+        String errorStr = ModServerConfig.getConfigError();
+        FormattedText errorText = FormattedText.of(errorStr, Style.EMPTY.withColor(ERROR_TEXT_COLOR));
+        int centeredX = this.width / 2;
 
-        gui.drawCenteredString(this.getFont(), title, width / 2, 20, 0xffffff);
-        gui.drawCenteredString(this.getFont(), desc_line_one, width / 2,
-                height / 2 - 80, 0xffffff);
-        gui.drawCenteredString(this.getFont(), desc_line_two, width / 2,
-                height / 2 - 65, 0xffffff);
-        gui.drawCenteredString(this.getFont(), desc_line_three, width / 2,
-                height / 2 - 50, 0xffffff);
-        //context.drawTextWithShadow(textRenderer, multilineError, height / 2 - 30, 16, 30);
-
-        //multilineError.draw(Alignment.LEFT, , DrawnTextConsumer);
+        gui.drawCenteredString(this.font, this.title, centeredX, 40 - this.font.lineHeight - 10, DEFAULT_TEXT_COLOR);
+        gui.drawCenteredString(this.font, desc_line_one, centeredX, 60 - this.font.lineHeight - 10, DEFAULT_TEXT_COLOR);
+        gui.drawCenteredString(this.font, desc_line_two, centeredX, 70 - this.font.lineHeight - 10, DEFAULT_TEXT_COLOR);
+        gui.drawCenteredString(this.font, desc_line_three, centeredX, 80 - this.font.lineHeight - 10, DEFAULT_TEXT_COLOR);
+        gui.drawCenteredString(this.font, desc_line_four, centeredX, 90 - this.font.lineHeight - 10, DEFAULT_TEXT_COLOR);
+        gui.drawWordWrap(this.font, errorText, centeredX - 80, 110 - this.font.lineHeight - 10, 200, DEFAULT_TEXT_COLOR);
     }
 
     @Override
     protected void init() {
-        this.closeButton = Button.builder(Component.translatable("config_error."
-                                + AssortedDiscoveries.MOD_ID + ".screen.close_button"),
+        int buttonWidth = 100;
+        int buttonHeight = 20;
+        int gap = 10;
+        int totalWidth = (buttonWidth * 2) + gap;
+        int startX = (this.width - totalWidth) / 2;
+        int yPos = (this.height / 2) + 30;
+
+        this.goBackButton = Button.builder(Component.translatable("config_error."
+                                + AssortedDiscoveries.MOD_ID + ".screen.go_back_button"),
                         button -> this.onClose())
-                .bounds(width / 2 - 50, height / 4 + height / 2 + 40, 100, 20).build();
-        this.addRenderableWidget(closeButton);
+                .bounds(startX, yPos, buttonWidth, buttonHeight).build();
+        this.addRenderableWidget(goBackButton);
+
+        this.continueButton = Button.builder(Component.translatable("config_error."
+                                + AssortedDiscoveries.MOD_ID + ".screen.continue_button"),
+                        button -> this.minecraft.setScreen(ModConfigScreen.getConfigScreenBuilder()
+                                .setParentScreen(this.parent).build()))
+                .bounds(startX + buttonWidth + gap, yPos, buttonWidth, buttonHeight).build();
+        this.addRenderableWidget(continueButton);
     }
 
     @Override

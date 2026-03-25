617c42f251b6e783535d16e0fcdb4bdcd50bc923
rndm_access <rndm.access.dev@gmail.com>
2026-03-25 09:09:05 -0600

    Improved the documentation again!

--- docs/js/side_page.js
+++ docs/js/side_page.js
@@ -30,26 +30,39 @@ function isVowel(char) {
 }
 
 /**
- *
  * The back HTML class must be on the crafting background image. This is used to update the alt text.
  * The cycle-item HTML class must be on the items you would like to cycle!
  *
- * {a}: Can be used in a data-alt-pattern or data-src-pattern to insert a or an into the string dynamically.
+ * {a}: Dynamically fills in 'a' or 'an' based on whether the current {blockType} starts with a vowel.
  *
- * {blockType}: Can be used in a data-alt-pattern or data-src-pattern to insert the item name from the list of items passed.
+ * {blockType}: Can be used in the data-alt-pattern or data-src-pattern to insert the item name from the list of items passed.
  *
  * data-alt-pattern and data-src-pattern should define the pattern of the new src and alt the cycle method
  * to use for the image and alt text.
  *
  * @example
- * data-alt-pattern="A Minecraft 3x3 crafting grid with seven {blockType} slabs arranged in a U-shape and one dirt block in the center, giving {a} {blockType} planter box, with planter box variations cycling."
- * This would be placed on the recipe gui background.
+ * <div class="crafting-area" id="overworld-crafting-gui">
+ *   <img class="back" src="./gui_images/crafting_table_display.png" data-alt-pattern="A Minecraft 3x3 crafting grid with seven {blockType} slabs arranged in a U-shape and one dirt block in the center, giving {a} {blockType} planter box, with planter box variations cycling." alt="A Minecraft 3x3 crafting grid with seven oak slabs arranged in a U-shape and one dirt block in the center, giving an oak planter box, with planter box variations cycling.">
+ *   <div class="front">
+ *     <div class="slot"><img class="crafting-item cycle-item" data-src-pattern="./image_renders/{blockType}_slab.png" src="image_renders/oak_slab.png" alt=""></div>
+ *     <div class="slot"></div>
+ *     <div class="slot"><img class="crafting-item cycle-item" data-src-pattern="./image_renders/{blockType}_slab.png" src="image_renders/oak_slab.png" alt=""></div>
  *
- * data-src-pattern="./image_renders/{blockType}_slab.png"
- * This would be placed on a gui item. This pattern defines a slab of {blockType} from the image_renders folder.
+ *     <div class="slot"><img class="crafting-item cycle-item" data-src-pattern="./image_renders/{blockType}_slab.png" src="image_renders/oak_slab.png" alt=""></div>
+ *     <div class="slot"><img class="crafting-item" src="image_renders/dirt.png" alt=""></div>
+ *     <div class="slot"><img class="crafting-item cycle-item" data-src-pattern="./image_renders/{blockType}_slab.png" src="image_renders/oak_slab.png" alt=""></div>
+ *
+ *     <div class="slot"><img class="crafting-item cycle-item" data-src-pattern="./image_renders/{blockType}_slab.png" src="image_renders/oak_slab.png" alt=""></div>
+ *     <div class="slot"><img class="crafting-item cycle-item" data-src-pattern="./image_renders/{blockType}_slab.png" src="image_renders/oak_slab.png" alt=""></div>
+ *     <div class="slot"><img class="crafting-item cycle-item" data-src-pattern="./image_renders/{blockType}_slab.png" src="image_renders/oak_slab.png" alt=""></div>
+ *   </div>
+ *   <div class="front-result">
+ *     <div><img class="result-item cycle-item" data-src-pattern="./image_renders/{blockType}_planter_box.png" src="./image_renders/oak_planter_box.png" alt=""></div>
+ *   </div>
+ * </div>
  *
  * @param items type list<string>: This should be the item names to cycle through.
- * @param guiId type Element: This should be the id for that recipe.
+ * @param guiId type string (ID): This should be the id for that recipe.
  * The id should be on the outermost parent element surrounding the recipe.
  */
 function createRecipeCycle(items, guiId) {
@@ -64,9 +77,33 @@ function createRecipeCycle(items, guiId) {
     }, 1000);
 }
 
-function createButtonPanel(e) {
+/**
+ * One button must start with id="selected". This ID is automatically moved to the newly clicked button.
+ * Each button must be labeled with the menu-btn class to be considered.
+ *
+ * @example
+ * HTML:
+ * <div id="button-container">
+ *     <button class="menu-btn" id="selected" data-type="oak" data-src-pattern="./image_renders/{type}_planter_box.png" data-alt-pattern="A picture of {a} {type} planter box">Oak</button>
+ *     <button class="menu-btn" data-type="spruce" data-src-pattern="./image_renders/{type}_planter_box.png" data-alt-pattern="A picture of {a} {type} planter box">Spruce</button>
+ * </div>
+ *
+ * JS:
+ * const container = document.getElementById('button-container');
+ * container.addEventListener('click', (event) => {
+ *   createButtonPanel(event)
+ * });
+ *
+ * {a}: Can be used in the data-alt-pattern to fill in 'a' or 'an' dynamically!
+ *
+ * {type}: Can be used in data-src-pattern or data-alt-pattern to dynamically insert a word!
+ * Underscores used in the substitution will automatically be replaced by spaces in the alt text.
+ *
+ * @param event type PointerEvent: The event object from the click event!
+ */
+function createButtonPanel(event) {
     const selectedElement = document.getElementById('selected')
-    const button = e.target.closest('.menu-btn');
+    const button = event.target.closest('.menu-btn');
     if (!button) return;
 
     const type = button.dataset.type;

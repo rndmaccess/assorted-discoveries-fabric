1d2a7cbdb99746e126417622e44fcea050b3d5a2
rndm_access <rndm.access.dev@gmail.com>
2026-03-26 17:43:53 -0600

    It's ugly but hopefully webpack doesn't remove my methods now.

--- src/js/side_page.js
+++ src/js/side_page.js
@@ -132,3 +132,6 @@ export function createButtonPanel(event) {
     selectedElement.removeAttribute('id');
     button.setAttribute('id', 'selected');
 }
+
+document.createButtonPanel = createButtonPanel
+document.createRecipeCycle = createRecipeCycle

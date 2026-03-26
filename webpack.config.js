a5d5885ce6c78934dd9f45798ba19d2c7f11e270
rndm_access <rndm.access.dev@gmail.com>
2026-03-25 22:18:41 -0600

    Let's try this

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
             entry: {
                 // Your HTML file is now the true entry point
                 index: 'src/index.html',
-                ...sync('src/pages/**/*.html').reduce((entries, file) => {
+                about: sync('src/pages/**/*.html').reduce((entries, file) => {
                     const name = path.basename(file, '.html');
                     entries[name] = file;
                     return entries;

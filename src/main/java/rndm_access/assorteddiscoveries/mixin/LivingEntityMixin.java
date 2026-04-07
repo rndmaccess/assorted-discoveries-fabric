943eed3dcc00d4f7ee564a0f2c5f58de6341c20c
rndm_access <rndm.access.dev@gmail.com>
2026-04-06 22:32:20 -0600

    Removed the unused method in the LivingEntityMixin

--- src/main/java/rndm_access/assorteddiscoveries/mixin/LivingEntityMixin.java
+++ src/main/java/rndm_access/assorteddiscoveries/mixin/LivingEntityMixin.java
@@ -4,15 +4,12 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-    @Shadow protected abstract int calculateFallDamage(double fallDistance, float damagePerDistance);
-
     @Inject(method = "causeFallDamage", at = @At("HEAD"), cancellable = true)
     public void causeFallDamage(double fallDistance, float damagePerDistance, DamageSource damageSource,
                                  CallbackInfoReturnable<Boolean> info) {

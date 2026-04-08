e7048ad2ee13184c1f2eda6de807776920300f6e
rndm_access <rndm.access.dev@gmail.com>
2026-04-07 22:43:55 -0600

    Optimized the logic for bone mealing endstone!

--- src/main/java/rndm_access/assorteddiscoveries/mixin/BoneMealItemMixin.java
+++ src/main/java/rndm_access/assorteddiscoveries/mixin/BoneMealItemMixin.java
@@ -2,6 +2,7 @@ package rndm_access.assorteddiscoveries.mixin;
 
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.ParticleUtils;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.gameevent.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +13,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import rndm_access.assorteddiscoveries.core.ModBlockTags;
 import rndm_access.assorteddiscoveries.core.ModBlocks;
 
-import java.util.Random;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -28,21 +28,21 @@ public abstract class BoneMealItemMixin {
 
     @Inject(method = "useOn", at = @At("HEAD"), cancellable = true)
     private void useOn(UseOnContext context, CallbackInfoReturnable<InteractionResult> info) {
-        BlockPos clickedPos = context.getClickedPos();
+        BlockPos soilPos = context.getClickedPos();
         Level level = context.getLevel();
-        boolean isBoneMealable = level.getBlockState(clickedPos).is(ModBlockTags.END_BONE_MEALABLE_BLOCKS);
-        BlockPos testPos = clickedPos.above();
-        boolean isEmptyAbove = level.getBlockState(testPos).isAir();
+        boolean isBoneMealable = level.getBlockState(soilPos).is(ModBlockTags.END_BONE_MEALABLE_BLOCKS);
+        BlockPos centerPos = soilPos.above();
+        boolean isEmptyAbove = level.getBlockState(centerPos).isAir();
 
         // Grow snapdragons and ender grass on blocks in the END_BONE_MEALABLE_BLOCKS when using bone meal.
-        if (isBoneMealable && !level.isOutsideBuildHeight(testPos) && isEmptyAbove) {
-            applyBoneMeal(context, level, clickedPos);
+        if (isBoneMealable && !level.isOutsideBuildHeight(centerPos) && isEmptyAbove) {
+            applyBoneMeal(context, level, centerPos);
             info.setReturnValue(InteractionResult.SUCCESS);
         }
     }
 
     @Unique
-    private static void applyBoneMeal(UseOnContext context, Level level, BlockPos clickedPos) {
+    private static void applyBoneMeal(UseOnContext context, Level level, BlockPos centerPos) {
         Player player = context.getPlayer();
         ItemStack boneMealStack = context.getItemInHand();
 
@@ -50,57 +50,62 @@ public abstract class BoneMealItemMixin {
             if (player != null) {
                 boneMealStack.causeUseVibration(player, GameEvent.ITEM_INTERACT_FINISH);
             }
-            growEnderPlants(level, clickedPos);
+            growEnderPlants(level, centerPos);
         }
-        spawnGrowthParticles(level, clickedPos);
+        spawnGrowthParticles(level, centerPos);
 
         boneMealStack.shrink(1);
-        level.playSound(null, clickedPos, SoundEvents.BONE_MEAL_USE, SoundSource.BLOCKS);
+        level.playSound(null, centerPos, SoundEvents.BONE_MEAL_USE, SoundSource.BLOCKS);
     }
 
     @Unique
-    private static void spawnGrowthParticles(Level level, BlockPos pos) {
+    private static void spawnGrowthParticles(final Level level, final BlockPos pos) {
         int count = level.getRandom().nextInt(10);
-        BlockPos particlePos = pos.above();
-        ParticleUtils.spawnParticles(level, particlePos, count * 3, 3.0D, 1.0D,
+        ParticleUtils.spawnParticles(level, pos, count * 3, 3.0D, 1.0D,
                 false, ParticleTypes.HAPPY_VILLAGER);
     }
 
     @Unique
-    private static void growEnderPlants(Level world, BlockPos centerPos) {
-        Random random = new Random();
+    private static void growEnderPlants(Level level, BlockPos centerPos) {
+        RandomSource random = level.getRandom();
+        BlockPos.MutableBlockPos mutablePos = centerPos.mutable();
+        BlockPos.MutableBlockPos belowPos = centerPos.below().mutable();
 
         for (int i = 0; i < 128; ++i) {
             // Re-center the position on the block bone mealed.
-            BlockPos.MutableBlockPos mutablePos = centerPos.mutable();
+            mutablePos.set(centerPos);
 
             for (int j = 0; j < i / 16; ++j) {
                 int xOffset = random.nextInt(3) - 1;
                 int yOffset = (random.nextInt(3) - 1) * random.nextInt(3) / 2;
                 int zOffset = random.nextInt(3) - 1;
                 mutablePos.move(xOffset, yOffset, zOffset);
-                BlockPos pos = mutablePos.immutable();
+                boolean shouldPlace = random.nextFloat() > 0.5F; // This gives the placement a sparse look!
 
-                placeBlocks(world, random, pos);
+                if (shouldPlace) continue;
+
+                // Skip all the blocks that are not air!
+                BlockState state = level.getBlockState(mutablePos);
+                if (!state.isAir()) continue;
+
+                belowPos.set(mutablePos.getX(), (mutablePos.getY() - 1), mutablePos.getZ());
+                BlockState soilState = level.getBlockState(belowPos);
+
+                if (soilState.is(ModBlockTags.END_BONE_MEALABLE_BLOCKS)) {
+                    placeBlock(level, random, mutablePos);
+                }
             }
         }
     }
 
     @Unique
-    private static void placeBlocks(Level world, Random random, BlockPos pos) {
-        BlockState state = world.getBlockState(pos);
-        BlockState soilState = world.getBlockState(pos.below());
-        boolean canPlace = soilState.is(ModBlockTags.END_BONE_MEALABLE_BLOCKS) && state.isAir();
-        boolean shouldPlace = world.getRandom().nextFloat() <= 0.5F; // This gives the placement a sparse look!
+    private static void placeBlock(Level level, RandomSource random, BlockPos pos) {
+        boolean placeSnapdragon = random.nextFloat() <= 0.4F; // 40% chance
 
-        if (canPlace && shouldPlace) {
-            boolean placeSnapdragon = random.nextFloat() <= 0.4F; // 40% chance
-
-            if(placeSnapdragon) {
-                world.setBlockAndUpdate(pos, ModBlocks.SNAPDRAGON.defaultBlockState());
-            } else {
-                world.setBlockAndUpdate(pos, ModBlocks.SHORT_ENDER_GRASS.defaultBlockState());
-            }
+        if(placeSnapdragon) {
+            level.setBlockAndUpdate(pos, ModBlocks.SNAPDRAGON.defaultBlockState());
+        } else {
+            level.setBlockAndUpdate(pos, ModBlocks.SHORT_ENDER_GRASS.defaultBlockState());
         }
     }
 }

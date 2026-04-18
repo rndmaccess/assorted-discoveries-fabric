2762d6ffe4c99a05b341edb51d280641196996ba
rndm_access <rndm.access.dev@gmail.com>
2026-04-18 12:15:56 -0600

    Improved the search functionality

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -6,11 +6,15 @@ let miniSearch = new MiniSearch({
 const documents = [
     {
         id: 1,
-        title: 'bog_blossom'
+        title: 'bog blossom'
     },
     {
         id: 2,
         title: 'planter box'
+    },
+    {
+        id: 3,
+        title: 'rope ladder'
     }
 ]
 
@@ -18,34 +22,14 @@ miniSearch.addAll(documents)
 
 const searchInput = document.getElementById('search')
 const suggestionsList = document.getElementById('suggestion-list')
+const app = document.getElementById('app')
 
 const getSuggestions = (query) => {
-
     return miniSearch.autoSuggest(query, {
         prefix: true, // partial word matching
         fuzzy: 0.2    // allow minor misspellings
     }).slice(0, 5)
 }
-
-const renderSuggestions = (suggestions) => {
-    suggestionsList.innerHTML = suggestions.map(({ suggestion }) => {
-        return `<li class="suggestion">${suggestion}</li>`
-    }).join('\n')
-    suggestionsList.style.display = 'block'
-}
-
-// Typing into search bar updates search results and suggestions
-searchInput.addEventListener('input', () => {
-    const query = searchInput.value
-
-    const suggestions = (query.length > 1) ? getSuggestions(query) : []
-
-    if (suggestions.length === 0) {
-        suggestionsList.style.display = 'none'
-    } else {
-        renderSuggestions(suggestions)
-    }
-})
 
 const getSearchResults = (query) => {
     const searchOptions = {
@@ -54,6 +38,54 @@ const getSearchResults = (query) => {
     }
     return miniSearch.search(query, searchOptions)
 }
+
+const renderResults = (suggestions, results) => {
+    let suggestion_words = suggestions.map(suggestion => suggestion.suggestion)
+    let result_words = results.map(result => result.title)
+
+    suggestionsList.innerHTML = result_words.map(( result ) => {
+        let display_result = result
+
+        if (result === undefined) {
+            return ""
+        }
+
+        suggestion_words.forEach(term => {
+            if (result.toLowerCase().includes(term.toLowerCase())) {
+                display_result = result.replace(term, "<b>" + term + "</b>")
+            }
+        });
+
+        return `<li class="suggestion" data-value="${result}">${display_result}</li>`;
+    }).join('\n')
+    suggestionsList.style.display = 'block'
+}
+
+app.addEventListener('click', (e) => {
+    suggestionsList.style.display = 'none'; // Hide list
+});
+
+suggestionsList.addEventListener('click', (e) => {
+    const li = e.target.closest('.suggestion');
+    if (li) {
+        searchInput.value = li.dataset.value; // Get text from data attribute
+        suggestionsList.style.display = 'none'; // Hide list
+    }
+});
+
+// Typing into search bar updates search results and suggestions
+searchInput.addEventListener('input', () => {
+    const query = searchInput.value
+
+    const suggestions = (query.length > 1) ? getSuggestions(query) : []
+    const results = (query.length > 1) ? getSearchResults(query) : []
+
+    if (suggestions.length === 0) {
+        suggestionsList.style.display = 'none'
+    } else {
+        renderResults(suggestions, results)
+    }
+})
 
 searchInput.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {

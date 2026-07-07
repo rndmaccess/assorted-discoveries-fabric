8d35e13dcbcd0c7417270155ae5b7e2390fd7c88
rndm_access <rndm.access.dev@gmail.com>
2026-07-07 11:37:57 -0600

    Tweaked the bounce logic for the purple mushroom block!

--- src/main/java/rndm_access/assorteddiscoveries/mixin/EntityMixin.java
+++ src/main/java/rndm_access/assorteddiscoveries/mixin/EntityMixin.java
@@ -14,7 +14,9 @@ import rndm_access.assorteddiscoveries.core.ModBlocks;
 @Mixin(Entity.class)
 public abstract class EntityMixin {
     @Unique
-    private static final float JUMP_HEIGHT = 2.0F;
+    private static final float JUMP_HEIGHT = 1.5F;
+    @Unique
+    private static final float JUMP_BOOST = 2.0F;
 
     @Shadow
     public void setDeltaMovement(final double xd, final double yd, final double zd) {}
@@ -24,10 +26,11 @@ public abstract class EntityMixin {
     @Inject(method = "restituteMovementAfterCollisions", at = @At("HEAD"), cancellable = true)
     private void restituteMovementAfterCollisions(BlockState effectState, boolean xCollision, boolean zCollision,
                                                   Vec3 movement, CallbackInfo ci) {
-        // Make the purple mushroom blocks bouncy!!!
+        // Makes the purple mushroom blocks bouncy!!!
         if (effectState.is(ModBlocks.PURPLE_MUSHROOM_BLOCK) && !isCrouching()) {
             if(movement.y() < 0) {
-                setDeltaMovement(movement.x(), JUMP_HEIGHT, movement.z());
+                double jumpSpeed = Math.max(-movement.y() * JUMP_BOOST, JUMP_HEIGHT);
+                setDeltaMovement(movement.x(), jumpSpeed, movement.z());
             } else {
                 setDeltaMovement(movement.x(), movement.y(), movement.z());
             }

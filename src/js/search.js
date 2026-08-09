3a88f95deccaf124a6bafb9a49bd7371b7bb13d3
rndm_access <rndm.access.dev@gmail.com>
2026-08-09 12:50:16 -0600

    Added the green onion and green onion seeds pages to the search results!

--- src/js/search.js
+++ src/js/search.js
@@ -13,6 +13,8 @@ import redMooshroomPlushie from '../img/red_mooshroom_plushie.webp';
 import whiteSheepPlushie from '../img/sheep_plushie_white.webp';
 import hoglinPlushie from '../img/hoglin_plushie.webp';
 import wildGreenOnions from '../img/wild_green_onions.webp';
+import greenOnionSeeds from '../img/green_onion_seeds.webp';
+import greenOnion from '../img/green_onion.webp';
 
 let miniSearch = new MiniSearch({
     fields: ['title', 'key_words'],
@@ -139,6 +141,22 @@ const documents = [
         description: 'A plant that drops green onion seeds when broken.',
         link: './wild_green_onions.html',
         img: wildGreenOnions,
+    },
+    {
+        id: 16,
+        title: 'green onion seeds',
+        key_words: 'plant',
+        description: 'The seeds used to plant green onion crops.',
+        link: './green_onion_seeds.html',
+        img: greenOnionSeeds,
+    },
+    {
+        id: 17,
+        title: 'green onion',
+        key_words: 'plant seeds food',
+        description: 'A food item that can be eaten as is or used in crafting recipes.',
+        link: './green_onion.html',
+        img: greenOnion,
     },
 ];
 

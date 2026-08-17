03c6af078a6e02021520da7f2033b8f0625e086b
rndm_access <rndm.access.dev@gmail.com>
2026-08-17 12:27:28 -0600

    Added the nether berry juice page!

---
+++ src/js/nether_berry_juice.js
@@ -0,0 +1,27 @@
+import cindersnapBerryJuice from "../img/cindersnap_berry_juice.webp";
+import frostbiteBerryJuice from "../img/frostbite_berry_juice.webp";
+
+// No module declaration needed; esbuild-loader handles imports.
+import { createButtonPanel } from "./side_page";
+
+const optionList = {
+    "cindersnap": [
+        {
+            "src": cindersnapBerryJuice,
+            "alt": "Cindersnap Berry Juice"
+        }
+    ],
+    "frostbite": [
+        {
+            "src": frostbiteBerryJuice,
+            "alt": "Frostbite Berry Juice"
+        }
+    ]
+}
+
+const headerImages = document.getElementsByClassName('header-image');
+const container = document.getElementById('button-container');
+
+container.addEventListener('click', (event) => {
+    createButtonPanel(event, optionList, container, headerImages);
+});
